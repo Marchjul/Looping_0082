8ca36cf621e6935a2d026228d4a632ffb4f79b23
Marchjul <marchellogiovani28@gmail.com>
2026-03-31 17:49:28 +0700

    pesan dan rerata

--- LogikaOR.cpp
+++ LogikaOR.cpp
@@ -5,4 +5,11 @@ using namespace std;
 int main(){
     float nilB, nilM, rerata;
     string status;
+
+    cout<<"Masukan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB + nilM) / 2;
 }
