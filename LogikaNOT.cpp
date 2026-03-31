2c15d1b3e3227e76a74948eb6635df752c901799
Marchjul <marchellogiovani28@gmail.com>
2026-03-31 17:46:22 +0700

    Meanampilkan pesan

--- LogikaNOT.cpp
+++ LogikaNOT.cpp
@@ -5,9 +5,14 @@ using namespace std;
 int main(){
     float nilB, nilM, rerata;
     string status;
-
-
-
-
     
+    cout<<"Masukan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+
+
+
+
 }
