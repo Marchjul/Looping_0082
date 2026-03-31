0fd0eff99dca0b295e47181e21814955163805f6
Marchjul <marchellogiovani28@gmail.com>
2026-03-31 17:47:00 +0700

    menampilkan status

--- LogikaNOT.cpp
+++ LogikaNOT.cpp
@@ -5,14 +5,18 @@ using namespace std;
 int main(){
     float nilB, nilM, rerata;
     string status;
-    
+
     cout<<"Masukan nilai Matematika = ";
     cin>>nilM;
     cout<<"Masukan nilai Bahasa Inggris = ";
     cin>>nilB;
 
+     rerata = (nilB + nilM) / 2;
 
+    if (!(rerata < 60))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
 
-
-
+    cout<<"Status kelulusan = "<<status<<", dengan nilai rata-rata = "<<rerata<<endl;
 }
